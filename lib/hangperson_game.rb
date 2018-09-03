f115562537be6ea7321d4d1a6d383198b49a91bf
Diego Camacho <hackvan@gmail.com>
2018-09-03 13:59:14 -0500

    Make the logic for hangperson game

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,20 +1,53 @@
 class HangpersonGame
 
-  # add the necessary class methods, attributes, etc. here
-  # to make the tests in spec/hangperson_game_spec.rb pass.
+  attr_accessor :word, :guesses, :wrong_guesses
 
-  # Get a word from remote "random word" service
-
-  # def initialize()
-  # end
-  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def guess(letter)
+    if letter.nil? || letter.empty? || letter !~ /[a-z]/i
+      raise ArgumentError
+    end
+    
+    letter_downcase = letter.downcase
+    return false if @guesses.include?(letter_downcase) || @wrong_guesses.include?(letter_downcase)
+
+    return @word.include?(letter_downcase) ? @guesses << letter_downcase : @wrong_guesses << letter_downcase
+  end
+
+
+  def word_with_guesses
+    word_status = '-' * @word.size
+    @guesses.each_char do |guess_letter|
+      @word.each_char.with_index do |letter, index|
+        if guess_letter == letter
+          word_status[index] = guess_letter
+        end
+      end
+    end
+    return word_status
+  end
+
+  def check_win_or_lose
+    if @wrong_guesses.size >= 7
+      return :lose
+    elsif word_with_guesses.include?('-')
+      return :play
+    else
+      return :win
+    end
+  end
+  
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
   #  => "cooking"   <-- some random word
+  #
+  # curl --data '' http://watchout4snakes.com/wo4snakes/Random/RandomWord
+  # --data is necessary to force curl to do a POST rather than a GET.
   def self.get_random_word
     require 'uri'
     require 'net/http'
